db9d5bbac8418ddc026c56ed656b6723693f0ef1
Anant Kumar <anantkumar012002@gmail.com>
2026-01-20 12:32:06 +0530

    Add AppShell widget for navigation structure

---
+++ lib/features/navigation/app_navigation.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:store/core/common_widgets/main_app_nav.dart';
+import 'package:store/features/home/presentation/screens/home_page.dart';
+
+class AppShell extends StatefulWidget {
+  const AppShell({super.key});
+
+  @override
+  State<AppShell> createState() => _AppShellState();
+}
+
+class _AppShellState extends State<AppShell> {
+  int _index = 0;
+  final _pages = const [
+    HomePage(),
+    // ProductsPage(),
+    // CartPage(),
+    // AccountPage(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(index: _index, children: _pages),
+      bottomNavigationBar: AppBottomNav(
+        currentIndex: _index,
+        onTap: (value) => setState(() => _index = value),
+      ),
+    );
+  }
+}
