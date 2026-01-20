809324a72537de5e478f200ce58c7a4a6326548f
Anant Kumar <anantkumar012002@gmail.com>
2026-01-20 11:57:08 +0530

    Fix import statement in widget test to reference app.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:store/main.dart';
+import 'package:store/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
