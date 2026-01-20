1b36f828b05cc9158e34aa938d3abfb62a54e419
Anant Kumar <anantkumar012002@gmail.com>
2026-01-20 13:43:22 +0530

    Refactor AccountPage to StatefulWidget and update navigation to include AccountPage

--- lib/features/account/presentation/screens/account_page.dart
+++ lib/features/account/presentation/screens/account_page.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
-class AccountPage extends StatelessWidget {
+class AccountPage extends StatefulWidget {
   const AccountPage({super.key});
+
+  @override
+  State<AccountPage> createState() => _AccountPageState();
+}
+
+class _AccountPageState extends State<AccountPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Account Page')),
       body: const Center(child: Text('Welcome to the Account Page!')),
     );
   }
--- lib/features/navigation/app_navigation.dart
+++ lib/features/navigation/app_navigation.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:store/core/common_widgets/main_app_nav.dart';
+import 'package:store/features/account/presentation/screens/account_page.dart';
 import 'package:store/features/home/presentation/screens/home_page.dart';
 
 class AppShell extends StatefulWidget {
@@ -11,7 +12,12 @@ class AppShell extends StatefulWidget {
 
 class _AppShellState extends State<AppShell> {
   int _index = 0;
-  final _pages = const [HomePage()];
+  final _pages = const [
+    HomePage(),
+    Center(child: Text('Cart Page')), // Placeholder for Cart
+    AccountPage(), // Profile page
+    Center(child: Text('Menu Page')), // Placeholder for Menu
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
